f775af0f7cf7d2902c1a2d24ef4c870a00f906a1
GabrieleOx <149193009+GabrieleOx@users.noreply.github.com>
2025-07-08 09:52:00 +0200

    Android v1.2
    Aggiunta versione 1.2 per android (Apk)

--- Android/AnimeDownloader/app/src/main/java/com/me/animedownloader/AnimeDownloader.kt
+++
@@ -1,8 +0,0 @@
-package com.me.animedownloader
-
-
-fun getSingleEp(episodeUrl: String): String {
-    var j = episodeUrl.length - 4
-    while (episodeUrl[j - 1] != '/') j--
-    return episodeUrl.substring(j, episodeUrl.length - 4)
-}
